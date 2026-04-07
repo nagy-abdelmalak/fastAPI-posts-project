fdbe87ce15d0e583ff61720161863aa954eb0d59
Nagy Abdelmalak <nagyabdelmalak@Nagys-MacBook-Pro.local>
2026-04-07 11:25:43 +0200

    1. array of posts with id (for database purposes) created /n2. get /posts/{id} to get a specific post using find_post function/n3. new posts are now saved correctly with a random unique id

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Optional
+from random import randrange
 
 app = FastAPI()
 
@@ -11,17 +12,32 @@ class Post(BaseModel):
     isPublic: bool = True
     rate: Optional[int] = None
 
+saved_posts = [{"title": "Post 1", "content": "Content 1", "id": 1},
+               {"title": "Post 2", "content": "Content 2", "id": 2}]
+
+def find_post(id: int):
+    for p in saved_posts:
+        if p["id"] == id:
+            return p
+
 # the root of the web app
 @app.get("/")
 def root():
     return {"message": "Welcome to my first FastAPI application!"}
 
-# a sub page of the app
+# a sub page of the app to display the posts
 @app.get("/posts")
 def posts():
-    return {"data": "Here are your posts"}
+    return {"data": saved_posts}
 
-# first post http request (expecting data from user in certain format to create and save correctly)
-@app.post("/createpost")
+@app.get("/posts/{id}")
+def post(id: int):
+    return find_post(id)
+
+# first post http request to create posts (expecting data from user in certain format to create and save correctly)
+@app.post("/posts")
 def create_post(payload: Post):
-    return {"message": f"Post {payload.title} created successfully!"}
+    post_dict = payload.dict()
+    post_dict["id"] = randrange(0, 10000000)
+    saved_posts.append(post_dict)
+    return {"data": post_dict}
